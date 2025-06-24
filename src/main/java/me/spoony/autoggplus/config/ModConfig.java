8a446351d5543d980547a3028cb14f28de36d049
SpoonySimone <92180945+SpoonySimone@users.noreply.github.com>
2025-06-24 16:23:39 +0200

    last config change i swear

--- src/main/java/me/spoony/autoggplus/config/ModConfig.java
+++ src/main/java/me/spoony/autoggplus/config/ModConfig.java
@@ -21,14 +21,6 @@ public class ModConfig extends Config {
     )
     public static boolean ModEnabled = true;
 
-    @Info(
-            text = "If you're using 'Randomize GG message', it's better to choose 'Good Game' or 'Good Fight' as they have more characters to randomize.",
-            subcategory = "Message Options",
-            size = 2,
-            type = InfoType.INFO // Types are: INFO, WARNING, ERROR, SUCCESS
-    )
-    public static boolean ignored;
-
     @Switch(
             name = "Randomize GG message",
             subcategory = "Message Options",
@@ -44,6 +36,15 @@ public class ModConfig extends Config {
             step = 1
     )
     public static int delay = 1;
+
+    @Info(
+            text = "If you're using 'Randomize GG message', it's better to choose 'Good Game' or 'Good Fight' as they have more characters to randomize.",
+            subcategory = "Message Options",
+            size = 2,
+            type = InfoType.INFO // Types are: INFO, WARNING, ERROR, SUCCESS
+    )
+    public static boolean ignored;
+
 
     @Dropdown(
             name = "GG Message",
